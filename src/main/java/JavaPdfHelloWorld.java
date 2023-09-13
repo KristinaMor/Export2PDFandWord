6a375b4b53a22953fddbbf84664eb8803cb33e70
jan.zitniak <jan.zitniak@gmail.com>
2023-09-13 21:15:28 +0200

    Optimalizácia zdrojového kódu, oprava generovania PDF s podporou slovenskej diakritiky.

--- src/main/java/JavaPdfHelloWorld.java
+++ src/main/java/JavaPdfHelloWorld.java
@@ -1,28 +1,34 @@
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import com.itextpdf.text.Document;
-import com.itextpdf.text.DocumentException;
-import com.itextpdf.text.Paragraph;
+import java.io.IOException;
+
+import com.itextpdf.text.*;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.pdf.BaseFont;
 import com.itextpdf.text.pdf.PdfWriter;
 
 public class JavaPdfHelloWorld
 {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Document document = new Document();
         try
         {
+            // -START- Inicializacia spravneho fontu
+            BaseFont baseFont = null;
+            baseFont = BaseFont.createFont("Sacramento-Regular.ttf", BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
+            Font font = new Font(baseFont, 12);
+            // -END- Inicializacia spravneho fontu
             PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream("HelloWorld.pdf"));
             document.open();
-            document.add(new Paragraph("A Hello World PDF document."));
+            document.add(new Paragraph("A Hello World PDF document.šččťšžčýáíé", font));
             document.close();
             writer.close();
-        } catch (DocumentException e)
-        {
-            e.printStackTrace();
-        } catch (FileNotFoundException e)
-        {
-            e.printStackTrace();
+        } catch (DocumentException e) {
+            System.out.println("Problém pri generovaní dokumentu");
+        } catch (FileNotFoundException e) {
+            System.out.println("Problém pri vytvarání súboru");
+        } catch (IOException e) {
+            System.out.println("Neznáma I/O chyba");
         }
     }
 }
